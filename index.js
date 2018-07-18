aa51ef17be706c056f156ef5858135f440446bd5
Paul Mestrum <paul.mestrum@delaware.pro>
2018-07-18 22:00:18 +0200

    add tabletop

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-() => {};
+function init() {
+//  key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
+  Tabletop.init({
+    key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
+    callback: function (data, tabletop) {
+      console.log(data)
+    },
+    simpleSheet: true
+  })
+}
+console.log('In index.js');
+init();
